480887457d997562bdddeadac0ee699552cd2a4d
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-08-18 09:32:05 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import HomePage from './pages/HomePage';
+import './style/App.css';
+import './style/reset.css';
+
+export default function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Home page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+        repudiandae expedita dignissimos adipisci nesciunt itaque debitis
+        tempora illo ipsa. Reprehenderit ratione ad temporibus modi rerum
+        laboriosam? Adipisci officiis rem id.
+      </p>
+    </div>
+  );
+}
